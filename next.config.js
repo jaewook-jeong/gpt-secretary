a122762866b2b9b9e2c6c26075d41dd1e42477d4
jaewook-jeong <dnrlwo11@gmail.com>
2023-03-15 17:20:47 +0900

    [CHORE] 불필요한 파일 제거

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa")({
 	disable: process.env.NODE_ENV === "development",
 	dest: "public",
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
 
 
 export default function App({ Component, pageProps }: AppProps) {
