13deaead34e8397d746fef141ddd4852be1bfd35
jaewook-jeong <dnrlwo11@gmail.com>
2023-03-15 15:36:58 +0900

    [CORE] 초기세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    emotion: {
+      sourceMap: true,
+      autoLabel: 'dev-only',
+    },
+  }
 }
 
-module.exports = nextConfig
+module.exports = withPlugins(
+	[
+		[
+			withPWA,
+			{
+				pwa: {
+					dest: "public",
+				},
+			},
+		],
+	],
+	nextConfig
+);
