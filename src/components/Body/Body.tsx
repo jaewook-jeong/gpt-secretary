6480d7d160dcf96c7fd88de4ec34f07177280cad
jaewook-jeong <dnrlwo11@gmail.com>
2023-03-17 10:49:13 +0900

    [FEAT] overflow-anchor 구현

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,23 +1,35 @@
+import { Fragment, useEffect, useRef } from 'react';
 import { useChattingStore } from '@/core/store';
 import { Wrapper, Date } from './Body.styled';
 import { Message } from './Message';
 import TypingSpinner from './Message/TypingSpinner';
 
 const Body = () => {
+  const scrollRef = useRef<HTMLElement>(null);
+  const messagesBodyRef = useRef<HTMLDivElement>(null);
   const { messages, isWaiting } = useChattingStore((state) => state);
 
+  useEffect(() => {
+    if (!messagesBodyRef.current) return;
+    if (!scrollRef.current) return;
+    scrollRef.current.scrollTop = messagesBodyRef.current.scrollHeight;
+  }, [isWaiting]);
+
   return (
-    <Wrapper>
-      {messages.map((message, index) => {
-        const isNewDate = index === 0 || message.sendAt.diff(messages[index - 1].sendAt, 'day') > 0;
-        return (
-          <>
-            {isNewDate && <Date>{message.sendAt.format('YYYY년MM월DD일')}</Date>}
-            <Message key={message.id} message={message} />
-          </>
-        );
-      })}
-      {isWaiting && <TypingSpinner />}
+    <Wrapper ref={scrollRef}>
+      <div ref={messagesBodyRef}>
+        {messages.map((message, index) => {
+          const isNewDate =
+            index === 0 || message.sendAt.diff(messages[index - 1].sendAt, 'day') > 0;
+          return (
+            <Fragment key={message.id}>
+              {isNewDate && <Date>{message.sendAt.format('YYYY년MM월DD일')}</Date>}
+              <Message message={message} />
+            </Fragment>
+          );
+        })}
+        {isWaiting && <TypingSpinner />}
+      </div>
     </Wrapper>
   );
 };
