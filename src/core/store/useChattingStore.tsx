2b9f0639cd731d1ec3a3cd6c0f3f4adb769ce041
jaewook-jeong <dnrlwo11@gmail.com>
2023-03-17 10:51:11 +0900

    [FIX] 최초 메세지 워딩 수정

--- src/core/store/useChattingStore.tsx
+++ src/core/store/useChattingStore.tsx
@@ -22,7 +22,7 @@ export const useChattingStore = create(
         id: 'HELLO_CHRIS',
         sendAt: dayjs(),
         sender: 'system',
-        body: '안녕하세요! 개발자 정재욱에 대해 알려드리는 AI비서입니다🧑🏽‍💻\n 답변에 다소 시간이 걸릴 수 있습니다. \n 무엇을 도와드릴까요?',
+        body: '안녕하세요! 개발자 정재욱에 대해 알려드리는 AI비서입니다🧑🏽‍💻\n 한국어 질문에는 이상한 답변을 할 수도 있습니다. \n 답변에 다소 시간이 걸릴 수 있습니다. \n 그러면 무엇을 도와드릴까요?',
       },
     ],
     isWaiting: false,
